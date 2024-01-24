28ed1ddd5886a45206c9ec13677f79eae6ccf54a
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2024-01-24 15:04:56 +0200

    Add files via upload

---
+++ Merkle–Hellman knapsack/Merkle–Hellman knapsack/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Merkle_Hellman_knapsack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 10.2F);
+            this.textBox1.Location = new System.Drawing.Point(128, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 27);
+            this.textBox1.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Private key:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(227, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 49);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 49);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Output:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(93, 310);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(463, 100);
+            this.richTextBox2.TabIndex = 25;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(93, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(463, 100);
+            this.richTextBox1.TabIndex = 24;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Input:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 10.2F);
+            this.textBox2.Location = new System.Drawing.Point(164, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 27);
+            this.textBox2.TabIndex = 31;
+            this.textBox2.Text = "8";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 18);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Number of bits:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(268, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "q:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(404, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 20);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "r:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 10.2F);
+            this.textBox3.Location = new System.Drawing.Point(301, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(72, 27);
+            this.textBox3.TabIndex = 35;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 10.2F);
+            this.textBox4.Location = new System.Drawing.Point(434, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(72, 27);
+            this.textBox4.TabIndex = 36;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(409, 245);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 49);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Generate key";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(17, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 18);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "Public key:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Consolas", 10.2F);
+            this.textBox5.Location = new System.Drawing.Point(128, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(428, 27);
+            this.textBox5.TabIndex = 39;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 438);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Merkle-Hellman Knapsack";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Merkle–Hellman knapsack/Merkle–Hellman knapsack/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Merkle_Hellman_knapsack
+{
+    public partial class Form1 : Form
+    {
+        string text;
+        string publickey;
+        string privatekey;
+        int q;
+        int r;
+        int n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Euclid(int n , int m)
+        {
+            while (n != m)
+                if (n > m)
+                    n -= m;
+                else
+                    m -= n;
+            return n;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid number of bits!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            if (n > 28)
+            {
+                MessageBox.Show("Number of bits is too large!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            text = richTextBox1.Text;
+            publickey = textBox5.Text;
+            char[] space = { ' ' };
+            string[] sirkey = publickey.Split(space, StringSplitOptions.RemoveEmptyEntries);
+            if (sirkey.Length != n)
+            {
+                MessageBox.Show("Number of bits doesnt match public key!");
+            }
+            else
+            {
+                int[] pubkey = new int[sirkey.Length];
+                for(int i=0; i<sirkey.Length; i++)
+                {
+                    pubkey[i] = Convert.ToInt32(sirkey[i]);
+                }
+                char[] sirtext = text.ToCharArray();
+                text = "";
+                for(int i=0; i<sirtext.Length; i++)
+                {
+                    if (sirtext[i]!=' ')
+                    {
+                        text+= Convert.ToString(sirtext[i], 2);
+                        text += ' ';
+                    }
+                }
+                string[] tokens = text.Split(space, StringSplitOptions.RemoveEmptyEntries);
+                text = "";
+                char[] sirtoken = new char[n];
+                char[] normalised = new char[n];
+                int poz;
+                int sum;
+                foreach(string token in tokens)
+                {
+                    sirtoken = token.ToCharArray();
+                    poz = n-sirtoken.Length;
+                    for(int i=0; i<sirtoken.Length; i++)
+                    {
+                        normalised[poz] = sirtoken[i];
+                        poz++;
+                    }
+                    poz=n-sirtoken.Length-1;
+                    while(poz!=-1)
+                    {
+                        normalised[poz]='0';
+                        poz--;
+                    }
+                    sum = 0;
+                    int a, b;
+                    for(int i=0; i<n; i++)
+                    {
+                        a = normalised[i] - '0';
+                        b = Convert.ToInt32(sirkey[i]);
+                        sum += a * b;
+                    }
+                    text += Convert.ToString(sum);
+                    text +=" ";
+                }
+                richTextBox2.Text = text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid number of bits!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            if (n > 28)
+            {
+                MessageBox.Show("Number of bits is too large!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            q = Convert.ToInt32(textBox3.Text);
+            r = Convert.ToInt32(textBox4.Text);
+            privatekey = textBox1.Text;
+            char[] space = { ' ' };
+            string[] sirkey = privatekey.Split(space, StringSplitOptions.RemoveEmptyEntries);
+            if (sirkey.Length != n)
+            {
+                MessageBox.Show("Number of bits doesnt match private key!");
+            }
+            else
+            {
+                int rprime=0;
+                for(int i=0; i<q; i++)
+                {
+                    if((r * i) % q == 1)
+                    {
+                        rprime = i;
+                        break;
+                    }
+                }
+                text = richTextBox1.Text;
+                string[] tokens = text.Split(space, StringSplitOptions.RemoveEmptyEntries);
+                int c=0, cprime=0;
+                string output = "";
+                double auxus = 0;
+                int outputsum = 0;
+                foreach(string token in tokens)
+                {
+                    c = Convert.ToInt32(token);
+                    cprime = (c * rprime) % q;
+                    for(int i=n-1; i>=0; i--)
+                    {
+                        int aux = int.Parse(sirkey[i]);
+                        if (aux <= cprime)
+                        {
+                            cprime -= aux;
+                            auxus = Math.Pow(2 ,n-(i+1));
+                            outputsum += Convert.ToInt32(auxus);
+                            if(cprime==0)
+                            {
+                                output += Convert.ToString(outputsum);
+                                output += " ";
+                                break;
+                            }
+                        }
+                    }
+                }
+                richTextBox2.Text = output;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid number of bits!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            if(n>28)
+            {
+                MessageBox.Show("Number of bits is too large!");
+                n = 8;
+                textBox2.Text = "8";
+            }
+            Random rnd= new Random();
+            int[] result = new int[n];
+            string W="";
+            result[0] = rnd.Next(1, 9);
+            int sum = result[0];
+            W += result[0].ToString();
+            W += " ";
+            for(int i=1; i<n; i++)
+            {
+                result[i] = rnd.Next(sum+1, sum + 15);
+                sum += result[i];
+                W += result[i].ToString();
+                W += " ";
+            }
+            textBox1.Text = W;
+            q = rnd.Next(sum + 1, sum + 150);
+            textBox3.Text = q.ToString();
+            int aux=0;
+            r = 0;
+            while(r==0)
+            {
+                aux = rnd.Next(10, q);
+                if(Euclid(q, aux)==1)
+                {
+                    r = aux;
+                    textBox4.Text = r.ToString();
+                }
+            }
+            string B = "";
+            for (int i = 0; i < n; i++)
+            {
+                B += ((result[i]*r)%q).ToString();
+                B += " ";
+            }
+            textBox5.Text = B;
+        }
+    }
+}
